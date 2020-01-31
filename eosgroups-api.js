c95f3cd1dfd8f7299e03e9c3a36723a18d7ef5d1
John Heeter <john@boid.com>
2020-01-31 03:29:53 -0600

    setup https

--- eosgroups-api.js
+++ eosgroups-api.js
@@ -2,6 +2,7 @@ const MongoWrapper = require('./classes/mongowrapper');
 const ApiServer = require('./classes/apiserver');
 const dfuse_provider = require('./classes/dfuse_provider');
 const config = require('./config/config.jungle.json');
-
+const setupProxy = require('./proxyServer')
 new dfuse_provider();
 new ApiServer(new MongoWrapper(config.mongo.url, config.mongo.dbname), config);
+setupProxy(443,'http://localhost:' + config.api.port)
